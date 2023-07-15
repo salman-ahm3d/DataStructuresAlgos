421f2f4fc82ed1c5fbbdb404382c5e44096f0592
salman-ahm3d <salmanahmed69890@gmail.com>
2023-07-15 13:34:26 -0600

    Update array.cpp to add functions and update code
    - Change default value of current_index to -1 and update functions to
      work with this change
    
    - Add searchElem(), searchSpecific(), searchBefore(), searchAfter() and
      insertStart() functions

--- src/array/array.cpp
+++ src/array/array.cpp
@@ -4,16 +4,84 @@
 
 using namespace std;
 
-int arr[MAX];
-int current_index = 0;
 
-void insertEnd(int elem) {
+int arr[MAX];
+int current_index = -1;
+
+
+
+int searchElem(int elem) {
+    if (current_index == -1) {
+        cout<<"Array empty!"<<endl;
+        return -1;
+    }
+    int returned_index = -1;
+    for (int i = 0; i<=current_index; i++) {
+        if (arr[i] == elem) {
+            return i;
+        }
+    }
+    return returned_index;
+}
+
+void searchBefore(int elem) {
+    int result = searchElem(elem);
+    if (result == -1) {
+        cout<<"Not found!"<<endl;
+    } else {
+        if (result == 0) {
+            cout<<"Cannot search before start"<<endl;
+        } else {
+            cout<<"Found "<<arr[result-1]<<" at index "<<result-1<<endl;
+        }
+    }
+}
+
+void searchAfter(int elem) {
+    int result = searchElem(elem);
+    if (result == -1) {
+        cout<<"Not found"<<endl;
+    } else {
+        if (result == current_index) {
+            cout<<"Cannot search after end"<<endl;
+        } else {
+            cout<<"Found "<<arr[result+1]<<" at index "<<result+1<<endl;
+            cout<<"CURRENT_INDEX="<<current_index<<endl;
+        }
+    }
+}
+
+void searchSpecific(int elem) {
+    int result = searchElem(elem);
+    if (result == -1) {
+        cout<<"Not found!";
+    } else {
+        cout<<"Found at index "<<result;
+    }
+    cout<<endl;
+}
+
+
+void insertStart(int elem) {
     if (current_index == MAX-1) {
         cout<<"Array full!"<<endl;
         return;
     } else {
-        arr[current_index] = elem;
         current_index++;
+        for (int i = current_index; i>0; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[0] = elem;
+    }
+}
+
+void insertEnd(int elem) {
+    if (current_index == MAX) {
+        cout<<"Array full!"<<endl;
+        return;
+    } else {
+        current_index++;
+        arr[current_index] = elem;
     }
 }
 
@@ -33,11 +101,11 @@ void insertElements() {
 }
 
 void displayElements() {
-    if (current_index == 0) {
+    if (current_index == -1) {
         cout<<"Array empty!"<<endl;
         return;
     } else {
-        for (int i=0; i<current_index; i++) {
+        for (int i=0; i<=current_index; i++) {
             cout<<arr[i]<<" ";
         }
     }
@@ -45,11 +113,11 @@ void displayElements() {
 }
 
 void emptyArray() {
-    if (current_index == 0) {
+    if (current_index == -1) {
         cout<<"Array already empty!"<<endl;
         return;
     } else {
-        current_index = 0;
+        current_index = -1;
     }
 }
 
@@ -65,9 +133,27 @@ void searchMenu() {
         cout<<"0. Back to main menu"<<endl;
         cin>>choice;
         switch (choice) {
-            case 1: break;
-            case 2: break;
-            case 3: break;
+            case 1:{
+                int elem;
+                cout<<"Enter element to be searched"<<endl;
+                cin>>elem;
+                searchSpecific(elem);
+                break;
+            } 
+            case 2: {
+                int elem;
+                cout<<"Enter element to search before"<<endl;
+                cin>>elem;
+                searchBefore(elem);
+                break;
+            } 
+            case 3: {
+                int elem;
+                cout<<"Enter element to search after"<<endl;
+                cin>>elem;
+                searchAfter(elem);
+                break;
+            }
             case 4: break;
             case 5: break;
             case 0: return;
@@ -116,8 +202,20 @@ void insertMenu() {
         cout<<"0. Back to main menu"<<endl;
         cin>>choice;
         switch (choice) {
-            case 1: break;
-            case 2: break;
+            case 1: {
+                int elem;
+                cout<<"Enter element to be inserted"<<endl;
+                cin>>elem;
+                insertStart(elem);
+                break;
+            } 
+            case 2: {
+                int elem;
+                cout<<"Enter element to be inserted"<<endl;
+                cin>>elem;
+                insertEnd(elem);
+                break;
+            } 
             case 3: break;
             case 4: break;
             case 5: break;
